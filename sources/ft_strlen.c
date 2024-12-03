cf65a58b15b413b6c5493d243ee7cf89c0861c1e
Michael Rouves <mrouves@student.42angouleme.fr>
2024-12-03 19:28:15 +0100

    protected strlen

--- sources/ft_strlen.c
+++ sources/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: mrouves <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 11:31:49 by mrouves           #+#    #+#             */
-/*   Updated: 2024/10/11 10:41:30 by mrouves          ###   ########.fr       */
+/*   Updated: 2024/12/03 19:27:51 by mrouves          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ size_t	ft_strlen(const char *s)
 {
 	const char	*ptr;
 
+	if (__builtin_expect(!s, 0))
+		return (0);
 	ptr = s;
 	while (*ptr)
 		ptr++;
