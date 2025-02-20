bdeb941a596339891c68fc11b04e1620fde87156
MykleR <michael.rouves@gmail.com>
2025-02-20 23:09:21 +0100

    stack_pops

--- headers/stack.h
+++ headers/stack.h
@@ -6,7 +6,7 @@
 /*   By: mrouves <mrouves@42angouleme.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 21:08:18 by mrouves           #+#    #+#             */
-/*   Updated: 2025/02/20 22:56:08 by mrouves          ###   ########.fr       */
+/*   Updated: 2025/02/20 23:08:27 by mrouves          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@
 typedef t_collection	t_stack;
 
 void	stack_push(t_stack *stack, void *data);
+void	stack_pops(t_stack *stack, uint32_t nb);
 void	*stack_pop(t_stack *stack);
 void	*stack_top(t_stack *stack);
 
--- sources/abstracts/stack.c
+++ sources/abstracts/stack.c
@@ -6,7 +6,7 @@
 /*   By: mrouves <mrouves@42angouleme.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 20:39:57 by mrouves           #+#    #+#             */
-/*   Updated: 2025/02/20 22:57:50 by mrouves          ###   ########.fr       */
+/*   Updated: 2025/02/20 23:09:07 by mrouves          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,12 @@
 void	stack_push(t_stack *stack, void *data)
 {
 	collection_append(stack, data);
+}
+
+void	stack_pops(t_stack *stack, uint32_t nb)
+{
+	while (stack->len && nb--)
+		stack_pop(stack);
 }
 
 void	*stack_pop(t_stack *stack)
