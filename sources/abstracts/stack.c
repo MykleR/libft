80b151a711c685e1fc910aed7c1ca974662bf82c
MykleR <michael.rouves@gmail.com>
2025-02-27 19:07:56 +0100

    patched stack pop

--- sources/abstracts/stack.c
+++ sources/abstracts/stack.c
@@ -6,7 +6,7 @@
 /*   By: mrouves <mrouves@42angouleme.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 20:39:57 by mrouves           #+#    #+#             */
-/*   Updated: 2025/02/20 23:09:07 by mrouves          ###   ########.fr       */
+/*   Updated: 2025/02/27 19:05:24 by mrouves          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,11 @@ void	stack_pops(t_stack *stack, uint32_t nb)
 
 void	*stack_pop(t_stack *stack)
 {
-	return (collection_get(stack, --stack->len));
+	void	*data;
+
+	data = collection_get(stack, stack->len - 1);
+	stack->len--;
+	return (data);
 }
 
 void	*stack_top(t_stack *stack)
