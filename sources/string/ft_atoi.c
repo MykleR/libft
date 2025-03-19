b6e66cd685402dec3f89ab92a6b271a7ed3c173b
MykleR <michael.rouves@gmail.com>
2025-03-19 14:16:59 +0100

    ok3

--- sources/string/ft_atoi.c
+++ sources/string/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: mrouves <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 11:34:18 by mrouves           #+#    #+#             */
-/*   Updated: 2025/03/19 14:09:04 by mrouves          ###   ########.fr       */
+/*   Updated: 2025/03/19 14:16:45 by mrouves          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,6 +51,8 @@ bool	ft_safe_atoi64(const char *s, int64_t	*out)
 
 	if (__builtin_expect(!out || !s || !(*s), 0))
 		return (false);
+	while ((*s >= '\t' && *s <= '\r') || *s == ' ')
+		s++;
 	sign = (*s != '-') - (*s == '-');
 	s += (*s == '-');
 	if (!ft_safe_atou64(s, &convert) || convert - (sign < 0) > INT64_MAX)
